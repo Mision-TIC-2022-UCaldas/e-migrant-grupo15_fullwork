41ede09366634d885563a08ce03949bb5fd19790
Marco Antonio Achury <67766219+marcoachury@users.noreply.github.com>
2021-11-27 01:27:43 -0500

    Enumeraciones

--- Migrante.App/Migrante.App.Dominio/Entidades/SituacionLaboral.cs
+++ Migrante.App/Migrante.App.Dominio/Entidades/SituacionLaboral.cs
@@ -3,6 +3,12 @@ namespace Migrante.App.Dominio
 {
     public enum SituacionLaboral
     {
-        Empleado, Desempleado, Independiente, EmpleadoATerminoFijo, EmpleadoPorPrestacionDeServicios, Pensionado, Otro
+        Empleado, 
+        Desempleado, 
+        Independiente, 
+        EmpleadoATerminoFijo, 
+        EmpleadoPorPrestacionDeServicios, 
+        Pensionado, 
+        Otro
     }
 }
