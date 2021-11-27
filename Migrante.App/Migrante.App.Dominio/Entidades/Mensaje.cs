10ccaa8ff9a33f290cf017d765f61745835a1cb8
Marco Antonio Achury <67766219+marcoachury@users.noreply.github.com>
2021-11-27 04:27:02 -0500

    Mensaje

--- Migrante.App/Migrante.App.Dominio/Entidades/Mensaje.cs
+++ Migrante.App/Migrante.App.Dominio/Entidades/Mensaje.cs
@@ -1,13 +1,15 @@
 using System;
 
-namespace Migrante.App.Dominio;
+namespace Migrante.App.Dominio{
 
-public class Mensaje{
-public int id {get;set;}
-public int timestamp {get;set;}
-public int id_remitente {get;set;}
-public int id_destinatario {get;set;}
-public int asuntocuerpo {get;set;}
-public boolean leido = false {get;set;}
+public class Mensaje {
+    public int id {get;set;}
+    public int timestamp {get;set;}
+    public int id_remitente {get;set;}
+    public int id_destinatario {get;set;}
+    public string asunto{get;set;}
+    public string cuerpo{get;set;}
 
-}
+    public Boolean leido {get;set;}
+
+}}
