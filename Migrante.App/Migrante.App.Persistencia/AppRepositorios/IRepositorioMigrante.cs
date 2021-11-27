f130ef921cf9a77d7e1056491b419b69e848b75c
Leonardo Jiménez <ljim230@gmail.com>
2021-11-27 01:29:05 -0500

    Implementación interfaces

---
+++ Migrante.App/Migrante.App.Persistencia/AppRepositorios/IRepositorioMigrante.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Migrante.App.Dominio;
+
+namespace Migrante.App.Persistencia
+{
+    public interface IRepositorioMigrante
+    {
+        //GetAllMigrantes
+        IEnumerable<PersonaMigrante> GetAllMigrantes();
+        //AddMigrante
+        PersonaMigrante AddMigrante (PersonaMigrante migrante);
+        //UpdateMigrante
+        PersonaMigrante UpdatePersonaMigrante(PersonaMigrante migrante);
+        //DeletePersonaMigrante
+        void DeletePersonaMigrante(int idMigrante);
+        //GetPersonaMigrante
+        PersonaMigrante GetPersonaMigrante(int idMigrante);
+        
+    }
+}
