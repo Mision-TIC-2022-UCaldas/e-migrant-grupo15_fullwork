0fe1fe6e9fe8992ff6c5fd6ef8ce48c2f366daf1
Leonardo Jiménez <ljim230@gmail.com>
2021-11-27 03:45:41 -0500

    (Reto 1) - Registrar Migrante

--- Migrante.App/Migrante.App.Persistencia/Migrations/20211127083841_SegundaMigracion.Designer.cs
+++ Migrante.App/Migrante.App.Persistencia/Migrations/20211127083841_SegundaMigracion.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Migrante.App.Persistencia;
 
 namespace Migrante.App.Persistencia.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20211127083841_SegundaMigracion")]
+    partial class SegundaMigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
