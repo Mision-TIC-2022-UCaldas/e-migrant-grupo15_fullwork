3053455819178a752f0fa5e66e6bcb71f7d7cca9
Marco Antonio Achury <67766219+marcoachury@users.noreply.github.com>
2021-11-28 20:59:25 -0500

    Reto Consulta de Migrante Completado

--- Migrante.App/Migrante.App.Frontend/Pages/Consultar.cshtml.cs
+++ Migrante.App/Migrante.App.Frontend/Pages/Consultar.cshtml.cs
@@ -11,11 +11,11 @@ namespace Migrante.App.Frontend.Pages
 {
     public class consultarModel : PageModel
     {
-/*
+
         private static IRepositorioMigrante _repoMigrante = new RepositorioMigrante(new Persistencia.AppContext());
 
         public PersonaMigrante personaMigrante { get; set; }
-*/
+
         public string docu = "Nueva cadena";
 
         //Este es el objeto recibido del formulario que llama la pagina
@@ -26,38 +26,34 @@ namespace Migrante.App.Frontend.Pages
         } //Se pueden Agregar los string que correspondan para cada campo del formulario
 
         //IActionResult
-        public void OnGet(DatosRecibidos DatosGet)
+        public IActionResult OnGet(DatosRecibidos DatosGet)
         {
             Console.WriteLine("Dentro del OnGet");
             docu = DatosGet.NumDocu;
             Console.WriteLine(docu);
             Console.WriteLine(value: "datos.NumDocu" + DatosGet.NumDocu );
             
-            /*
+            
             personaMigrante = _repoMigrante.GetPersonaMigrante_docu(DatosGet.NumDocu);
-            Console.WriteLine(personaMigrante.nombre);
-            Console.WriteLine(personaMigrante.apellidos);
-            Console.WriteLine(personaMigrante.numeroDocumento);
-*/
-            /*
-            if(personaMigrante.id == null)
+            
+            
+            if(personaMigrante == null)
              {
                  Console.Write("personaMigrante es null");
-                 //return RedirectToPage("/Index");
-
+                 return RedirectToPage("MigranteNoEncontrado");
              }
              else
              {
+
                  Console.WriteLine(personaMigrante.nombre);
                  Console.WriteLine(personaMigrante.apellidos);
                  Console.WriteLine(personaMigrante.numeroDocumento);
                  //Console.WriteLine(personaMigrante.nombre );
-
+                return null;
              }
-             */
+             
            
         }
     }
 }
-
 
