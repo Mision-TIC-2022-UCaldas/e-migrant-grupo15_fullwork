29e1e9165572e7a3b16b3fbd4762f0a18132028f
Marco Antonio Achury <67766219+marcoachury@users.noreply.github.com>
2021-11-29 02:22:07 -0500

    Formulario Registrar Entidad

--- Migrante.App/Migrante.App.Frontend/Pages/Entidades/RegistrarEntidad.cshtml.cs
+++ Migrante.App/Migrante.App.Frontend/Pages/Entidades/RegistrarEntidad.cshtml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Migrante.App.Dominio;
+using Migrante.App.Persistencia;
 
 namespace Migrante.App.Frontend.Pages.Entidades
 {
